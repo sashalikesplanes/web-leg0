b6ea4fa1e4f6398fb3add2a80b4db6accd86ee03
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-08-16 11:32:18 +0200

    made header component

--- frontend/website/src/routes/+layout.svelte
+++ frontend/website/src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script>
 	import '../app.postcss';
+	import Header from '$lib/components/header.svelte';
 </script>
 
+<Header />
+
 <main class="max-w-[850px] m-auto w-[97.5%]">
-
 <slot />
-
 </main>
