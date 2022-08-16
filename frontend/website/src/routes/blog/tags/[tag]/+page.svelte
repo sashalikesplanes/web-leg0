3ccf2f09698f3d3fc010efaec09c031a7663637b
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-08-16 16:52:56 +0200

    made blog posts by tag page

---
+++ frontend/website/src/routes/blog/tags/[tag]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import Icon from '$lib/components/icon.svelte';
+	export let data: PageData;
+</script>
+
+<div class="divider"/>
+<h1>Blog Posts tagged as <span class="underline">{data.tag}</span></h1>
+<div class="card w-full bg-base-200 hover:bg-base-300 shadow-xl">
+	<a href="/blog" class="card-body flex-row flex-wrap">
+		<Icon iconType="webrtc" classCss="h-8 w-8 md:h-16 md:w-16"/>
+		<span class="ml-4 flex-shrink card-title "><h2>WebRTC Chat server with WebSockets</h2></span>
+		<p class="basis-full text-primary">April 04th, 2022</p>
+	</a>
+</div>
+<div class="divider"/>
