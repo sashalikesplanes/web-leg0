c5f34b9a926d3a0355422ad318c8709a9889da76
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-09-25 19:51:47 +0200

    minor update

--- website/src/routes/about-me.svelte
+++ website/src/routes/about-me.svelte
@@ -1,10 +1,11 @@
 <div class="flex gap-4">
   <div>
-<p>Hello world! My name is <span class="text-accent">Sasha</span> (short from Alexander) and I am a full stack software engineer. I am about to complete a Bachelor in Aerospace Engineering at the Technical University of Delft (TU Delft). Software engineering is my career, my passion and my hobby.</p>
+<p>Hello world! My name is <span class="text-accent">Sasha</span> (short from Alexander) and I am a full stack software engineer. I am about to complete a Bachelor in Aerospace Engineering at the Technical University of Delft (TU Delft). Software engineering is my career, my passion and my hobby. I wish to use it to make a positive social impact on the world around me.</p>
 <p>I am interested in the following applications of software engineering:</p>
 <ul class="list-disc list-inside">
   <li>Smart management of power grids, to bring about the energy transition</li>
   <li>Communication and data-gathering satellites</li>
+  <li>Medical applications</li>
   <li>Scientific research</li>
 </ul>
 <p>Please reach out to me, if you believe you have a project which would suit one of my above stated interests and are in need of a quick-learning, independent and ambitious software engineer.</p>
